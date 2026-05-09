8e40ab849f67a992af6228c7f7043cdae2d4911c
aleezanoor845-png <aleezanoor845@gmail.com>
2026-05-09 19:04:29 +0500

    Added ReadNumber procedure - reads large number input

--- Code/input.asm
+++ Code/input.asm
@@ -1,2 +1,38 @@
-; Member 1 - Input Module
-; Procedures: ReadNumber, ValidateNumber
+; ============================================
+; Member 1: Aleeza Noor
+; Module: Input & Storage
+; File: input.asm
+; ============================================
+
+INCLUDE Irvine32.inc
+
+.data
+    prompt      BYTE "Enter a large number: ", 0
+    inputBuffer BYTE 210 DUP(0)
+    invalidMsg  BYTE "Invalid input! Try again.", 0
+    
+.code
+
+; ============================================
+; ReadNumber PROC
+; Purpose: Reads number as string from user
+; Input: nothing
+; Output: inputBuffer filled with user input
+; ============================================
+ReadNumber PROC
+    mov  edx, OFFSET prompt
+    call WriteString
+    
+    mov  edx, OFFSET inputBuffer
+    mov  ecx, 210
+    call ReadString
+    
+    ret
+ReadNumber ENDP
+
+main PROC
+    call ReadNumber
+    exit
+main ENDP
+
+END main
