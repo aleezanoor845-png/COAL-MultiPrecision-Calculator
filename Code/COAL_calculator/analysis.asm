0d7e326738f84040266bb084e7f3cebebc5f56c1
Urwa Abbasi <242703@students.au.edu.pk>
2026-05-11 16:33:27 -0700

    Initial MASM project setup with file structure

---
+++ Code/COAL_calculator/analysis.asm
@@ -0,0 +1,11 @@
+INCLUDE Irvine32.inc
+
+.code
+
+AnalyzeInput PROC
+
+    ret
+
+AnalyzeInput ENDP
+
+END
---
+++ Code/COAL_calculator/output.asm
@@ -0,0 +1,11 @@
+INCLUDE Irvine32.inc
+
+.code
+
+DisplayResult PROC
+
+    ret
+
+DisplayResult ENDP
+
+END
